b41d28f1dab9e5d70c1a06281fcc14db330c0b7a
loudufraneFS <loudufrane@fullsail.edu>
2014-08-07 18:33:45 -0500

    Array

--- Madlibs/main.py
+++ Madlibs/main.py
@@ -18,20 +18,22 @@ As three blind mice?
 punc = ''
 
 # Inputs - 3 strings and 3 numbers
-animal = raw_input("Type in animal in plural. i.e. pigs  ")
-occupation = raw_input("Type in a profession. i.e. Painter  ")
+animal = raw_input("Type in animal in plural:   ")
+occupation = raw_input("Type in a profession:  ")
 adjective = raw_input("Type in an adjective.  ")
 number = raw_input("Type in a number from 2 to 10 ")
-wives = raw_input("Type in any number ")
-knives = raw_input("Type in another number from 2 to 10 ")
+wives = raw_input("Type in any number:  ")
+knives = raw_input("Type in another number from 2 to 10:  ")
 
+#add number variables into array
+numbers_list = [number, knives, wives]
 
 #conditions
-if wives == 1:
+if int(wives) == 1:
     punc = 'st'
-elif wives == 2:
+elif int(wives) == 2:
     punc = 'nd'
-elif wives == 3:
+elif int(wives) == 3:
     punc = 'rd'
 else:
     punc = 'th'
