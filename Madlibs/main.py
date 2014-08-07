2c0bb5802f5d9a63039e8ed3f9bab4edcfdf3945
loudufraneFS <loudufrane@fullsail.edu>
2014-08-07 18:54:20 -0500

    dictionary

--- Madlibs/main.py
+++ Madlibs/main.py
@@ -14,8 +14,8 @@ punc = ''
 # Inputs - 3 strings and 4 numbers
 animal = raw_input("Type in animal in plural:   ")
 occupation = raw_input("Type in a profession:  ")
-adjective = raw_input("Type in an adjective.  ")
-number = raw_input("Type in a number from 2 to 10 ")
+adjective = raw_input("Type in an adjective:   ")
+number = raw_input("Type in a number from 2 to 10:  ")
 wives = raw_input("Type in any number:  ")
 knives = raw_input("Type in another number from 2 to 10:  ")
 year = raw_input ("Type in a year: ")
@@ -24,7 +24,7 @@ year = raw_input ("Type in a year: ")
 #add number variables into array
 numbers_list = [number, knives, wives, year]
 
-#calculate age
+#calculate age - using mathematical operator
 age = 2014 - int(numbers_list[3])
 
 #conditions
@@ -43,10 +43,13 @@ if (int(numbers_list[1]) == 0  or int(numbers_list[1]) == 1):
 else:
     knife = 'knives'
 
+#dictionary
+dict = {'sight': 'blind', 'action': 'run', 'years' : age}
+
 #story
-print str(numbers_list[0]) + " blind "  + animal + ", " + str(numbers_list[0]) + " blind "  + animal + "."
-print ", See how they run, see how they run."
-print "They all ran after the "+ occupation +"'s " + str(numbers_list[2]) + str(punc) + " wife."
+print str(numbers_list[0]) + " " + dict['sight'] + " " + animal + ", " + str(numbers_list[0]) + " " + dict['sight'] + " " + animal + "."
+print "See how they " + dict['action'] + ", see how they " + dict['action'] + "."
+print "They all ran after the "+ occupation +"'s " + str(numbers_list[2]) + str(punc) + " "  + dict['years'] +  " year old wife."
 print "Who cut off their tails with " + str(numbers_list[1]) + " " + adjective + " " + knife + "."
 print" Did you ever see such a thing in your life."
 print "As " + str(numbers_list[0]) + " blind " + animal
