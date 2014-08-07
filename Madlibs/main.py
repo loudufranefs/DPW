8ffdc0d85b8b44b24e398b492271163807370537
loudufraneFS <loudufrane@fullsail.edu>
2014-08-07 08:48:36 -0500

    Adding raw_input variables

--- Madlibs/main.py
+++ Madlibs/main.py
@@ -3,4 +3,20 @@ Name:Loubna Dufrane
 Date:08/07/1977
 Assignment: Madlibs
 Class: Design Patterns for Web Programming
+
+Three blind mice, three blind mice,
+See how they run, see how they run,
+They all ran after the farmer's wife,
+Who cut off their tails with a carving knife,
+Did you ever see such a thing in your life,
+As three blind mice?
+
 '''
+# Inputs - 4 strings and 3 numbers
+animal = raw_input("Type in animal in plural. i.e. pigs  ")
+occupation = raw_input("Type in a profession. i.e. Painter  ")
+adjective = raw_input("Type in an adjective.  ")
+verb = raw_input("Type in an verb.  ")
+number = raw_input("Type in a number")
+wives = raw_input("Type in another number from 4 to 10")
+knives = raw_input("Type in another number from 1 to 5 ")
