a2aeaa218c5d98712a71fd2d012fc0663825b51f
loudufraneFS <loudufrane@fullsail.edu>
2014-08-07 09:03:15 -0500

    Print Madlibs story with all required variables

--- Madlibs/main.py
+++ Madlibs/main.py
@@ -12,11 +12,13 @@ Did you ever see such a thing in your life,
 As three blind mice?
 
 '''
-# Inputs - 4 strings and 3 numbers
+# Inputs - 3 strings and 3 numbers
 animal = raw_input("Type in animal in plural. i.e. pigs  ")
 occupation = raw_input("Type in a profession. i.e. Painter  ")
 adjective = raw_input("Type in an adjective.  ")
-verb = raw_input("Type in an verb.  ")
-number = raw_input("Type in a number")
-wives = raw_input("Type in another number from 4 to 10")
-knives = raw_input("Type in another number from 1 to 5 ")
+number = raw_input("Type in a number ")
+wives = raw_input("Type in another number ")
+knives = raw_input("Type in another number  ")
+
+#initial story
+print str(number) + " blind "  + animal + ", " + str(number) + " blind "  + animal + ", See how they run, see how they run, They all ran after the "+ occupation +"'s " + str(wives) + "th wife, Who cut off their tails with " + knives + " " + adjective + " knives, Did you ever see such a thing in your life, As " + str(number) + " blind " + animal
