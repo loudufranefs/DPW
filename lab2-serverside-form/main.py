7b2e3bcb636dd9436c6ef8768ee83ffb1257b8f2
loudufraneFS <loudufrane@fullsail.edu>
2014-08-12 20:12:58 -0500

    checkbox

--- lab2-serverside-form/main.py
+++ lab2-serverside-form/main.py
@@ -6,7 +6,6 @@ Class: Design Patterns for Web Programming
 Concept : form for booking a flight
 '''
 import webapp2
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         page_header = '''<!DOCTYPE HTML>
@@ -21,21 +20,25 @@ class MainHandler(webapp2.RequestHandler):
         #display form content
         page_content_form = '''
         <form method="GET">
+            <div id="oneWayCheck"><label>One Way?</label><input type="checkbox" name="type"></div>
             <div><label>From</label><input type="text" name="from_location"></div>
-            <div><label>To</label><input type="text" name="to_location"></div>
+            <div id="to_location"><label>To</label><input type="text" name="to_location"></div>
             <div><label>Leaving</label><input type="date" name="leave_date"></div>
-            <div><label>Returning</label><input type="date" name="return_date"></div>
+            <div id="return_date"><label>Returning</label><input type="date" name="return_date"></div>
             <!-Select->
-            <div><label>Persons</label>
-            <select name="persons">
-                <option value="1" selected>1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select></div>
+            <div>
+                <label>Persons</label>
+                <select name="persons">
+                    <option value="1" selected>1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
         </form>
         '''
+
         #display result page content
         page_content_result = '''
         '''
