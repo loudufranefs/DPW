cc4bf07409a08acaf7ffcb74f9c7d03ccdaff237
loudufraneFS <loudufrane@fullsail.edu>
2014-08-21 06:32:57 -0500

    moving class to separate file

--- encapsulated/main.py
+++ encapsulated/main.py
@@ -6,22 +6,14 @@ Class: Design Patterns for Web Programming
 Concept : Weightloss Challenge
 '''
 import webapp2
+#import weightlostttracker
+from tracker import WeightLossTracker
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
-class WeightLossTracker(object)
-    def __init__(self)
-    self.week1 = 0
-    self.week2 = 0
-    self.week3 = 0
-    self.week4 = 0
-    self.__total_weightloss = 0
-    self.__avg_weighloss_week = 0
 
-    
-    
     
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
---
+++ encapsulated/tracker.py
@@ -0,0 +1,9 @@
+#Creating weightloss class holding the attributes
+class WeightLossTracker(object)
+    def __init__(self)
+    self.week1 = 0
+    self.week2 = 0
+    self.week3 = 0
+    self.week4 = 0
+    self.__total_weightloss = 0 #private attribute
+    self.__avg_weighloss_week = 0 #private attribute
