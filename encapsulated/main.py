7782bc4228e0ed9836409eb8f838468ff6ac0453
loudufraneFS <loudufrane@fullsail.edu>
2014-08-21 06:29:35 -0500

    initial class setup

--- encapsulated/main.py
+++ encapsulated/main.py
@@ -1,25 +1,28 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+'''
+Name: Loubna Dufrane
+Date: 9/12/2014
+Assignment encapsulated Calculator
+Class: Design Patterns for Web Programming
+Concept : Weightloss Challenge
+'''
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class WeightLossTracker(object)
+    def __init__(self)
+    self.week1 = 0
+    self.week2 = 0
+    self.week3 = 0
+    self.week4 = 0
+    self.__total_weightloss = 0
+    self.__avg_weighloss_week = 0
+
+    
+    
+    
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
