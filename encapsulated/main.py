710205e48c2eed368949de2b6d4b142fb929b6bc
loudufraneFS <loudufrane@fullsail.edu>
2014-08-21 07:54:00 -0500

    adding URL parameters for each object

--- encapsulated/main.py
+++ encapsulated/main.py
@@ -44,14 +44,28 @@ class MainHandler(webapp2.RequestHandler):
         paul.week3 = 8
         paul.week4 = 2
         
-       #self.response.write("Lucy lost and average of " + str(lucy.avg_weighloss_week) + " pounds per week,  and a total of " + str(lucy.total_weightloss) + " pounds.")
+        #Data Object - Nick
+        nick = WeightLossTracker()
+        nick.week1 = 3
+        nick.week2 = 4
+        nick.week3 = 8
+        nick.week4 = 3
         
-        p.page_body = "Lucy lost and average of " + str(lucy.avg_weighloss_week) + " pounds per week,  and a total of " + str(lucy.total_weightloss) + " pounds."
+        p.page_body = '''
+        <div class="challengers">
+            <ul>
+            <li>Lucy <a href="/?week1={lucy.week1}&week2={lucy.week2}&week3={lucy.week3}&week4={lucy.week4}&avg_weightlos={lucy.avg_weighloss_week}&total_weightloss={lucy.total_weightloss}">Lucy's Weighloss</a></li>
+            <li>Mark <a href="/?week1={mark.week1}&week2={mark.week2}&week3={mark.week3}&week4={mark.week4}&avg_weightlos={mark.avg_weighloss_week}&total_weightloss={mark.total_weightloss}"">Mark's Weighloss</a></li>
+            <li>Pam <a href="/?week1={pam.week1}&week2={pam.week2}&week3={pam.week3}&week4={pam.week4}&avg_weightlos={pam.avg_weighloss_week}&total_weightloss={pam.total_weightloss}"">Pam's Weighloss</a></li>
+            <li>Paul <a href="/?week1={paul.week1}&week2={paul.week2}&week3={paul.week3}&week4={paul.week4}&avg_weightlos={paul.avg_weighloss_week}&total_weightloss={paul.total_weightloss}"">Paul's Weighloss</a></li>
+            <li>Nick <a href="/?week1={nick.week1}&week2={nick.week2}&week3={nick.week3}&week4={nick.week4}&avg_weightlos={nick.avg_weighloss_week}&total_weightloss={nick.total_weightloss}"">Nick's Weighloss</a></li>
+            </ul>
+        </div>
+        '''
         
         print_page = p.page_head + p.page_body + p.page_end
-        self.response.write(print_page)
+        self.response.write(print_page.format(**locals()))
 
-    
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
