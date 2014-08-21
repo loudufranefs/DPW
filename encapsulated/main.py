0b62b4fe0f2275b7a9e1266aaff66dc92efdd688
loudufraneFS <loudufrane@fullsail.edu>
2014-08-21 07:31:12 -0500

    adding 4 more data objects

--- encapsulated/main.py
+++ encapsulated/main.py
@@ -16,14 +16,37 @@ class MainHandler(webapp2.RequestHandler):
         #page template
         p = PageTemplate()
         
-        #Data Object
+        #Data Object - Lucy
         lucy = WeightLossTracker()
         lucy.week1 = 5
         lucy.week2 = 8
         lucy.week3 = 2
         lucy.week4 = 4
         
+        #Data Object - Mark
+        mark = WeightLossTracker()
+        mark.week1 = 4
+        mark.week2 = 3
+        mark.week3 = 10
+        mark.week4 = 5
+        
+        #Data Object - Pam
+        pam = WeightLossTracker()
+        pam.week1 = 6
+        pam.week2 = 3
+        pam.week3 = 8
+        pam.week4 = 2
+        
+        #Data Object - Paul
+        paul = WeightLossTracker()
+        paul.week1 = 10
+        paul.week2 = 5
+        paul.week3 = 8
+        paul.week4 = 2
+        
        #self.response.write("Lucy lost and average of " + str(lucy.avg_weighloss_week) + " pounds per week,  and a total of " + str(lucy.total_weightloss) + " pounds.")
+        
+        p.page_body = "Lucy lost and average of " + str(lucy.avg_weighloss_week) + " pounds per week,  and a total of " + str(lucy.total_weightloss) + " pounds."
         
         print_page = p.page_head + p.page_body + p.page_end
         self.response.write(print_page)
