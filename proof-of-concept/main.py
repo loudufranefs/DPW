9a52565151d1271740c888e932ff57d72ddc360e
loudufraneFS <loudufrane@fullsail.edu>
2014-08-21 21:15:57 -0500

    building page in a variable

--- proof-of-concept/main.py
+++ proof-of-concept/main.py
@@ -10,7 +10,9 @@ from xml.dom import minidom #importing minidom of parsing
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        #instance of page template
+        p = PageTemplate()
+        self.response.write(p.whole_page)
         
 class PageTemplate(object):
     def __init__(self):
@@ -22,18 +24,17 @@ class PageTemplate(object):
     <title></title>
     </head>
     <body>
-    '''
+    <h1>Page title</h1>
+        '''
         #page content
-        self.page_content='''
-    '''
+        self.page_content=''
         #page end html
         self.page_end='''
     </body>
 </html>
-    '''
+        '''
     
-        def build_page(self):
-            return self.page_head + self.page_content + self.page_end
+        self.whole_page = self.page_head + self.page_content + self.page_end
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
