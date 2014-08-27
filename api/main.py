0acddbd981acd429cf040721b96233bcf0df76cf
loudufraneFS <loudufrane@fullsail.edu>
2014-08-26 19:51:44 -0500

    Setting up IP request and parsing information

--- api/main.py
+++ api/main.py
@@ -20,28 +20,31 @@ from xml.dom import minidom #importing minidom of parsing
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        
         
 
         #proof of concept code
         
         #get values from API url with hardcoded value
-        api_url = "http://api.hostip.info/?ip=12.215.42.19"
+        api_url = "http://ip-api.com/xml/208.80.152.201"
         #create request using urllib2 library for api url
         api_request = urllib2.Request(api_url)
         #create api object opener
         api_opener = urllib2.build_opener()
         #get info from api url
         api_result = api_opener.open(api_request)
-        
-        
         #use minidom to parse xml
         apixml = minidom.parse(api_result)
         
-        #target element named IP
-        ip = apixml.getElementsByTagName('ip')[0].firstChild.nodeValue
-        #target element named "countryName"
-        country_name = apixml.getElementsByTagName('countryName')[0].firstChild.nodeValue
+        #setting up variables for return values temporarily
+        timezone = apixml.getElementsByTagName('timezone')[0].firstChild.nodeValue
+        lon = apixml.getElementsByTagName('lon')[0].firstChild.nodeValue
+        lat = apixml.getElementsByTagName('lat')[0].firstChild.nodeValue
+        zipcode = apixml.getElementsByTagName('zip')[0].firstChild.nodeValue
+        city = apixml.getElementsByTagName('city')[0].firstChild.nodeValue
+        country = apixml.getElementsByTagName('country')[0].firstChild.nodeValue
+        
+        self.response.write(country)
         
 #VIEW
 #-display form
